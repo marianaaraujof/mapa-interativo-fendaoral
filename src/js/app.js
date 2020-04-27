bb97841536b5c31ccf9d887be0586f9714a3bbfc
Mariana Figueiredo <marianaaraujof@ufrn.edu.br>
2020-04-27 13:39:46 -0300

    hover dos estados

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,143 @@
 console.log('bla appjs');
 
 $(document).ready(function() {
+    
     $("#ba").hover( 
         function() {
         console.log('text');
-        var txt1 = document.createElement("div");
-            txt1.innerHTML = "Hospital Santo Antonio";
-            $(txt1).addClass("box-estado")
-        $( this ).append( txt1);
-    },
-    function() {
-        $( this ).find( 'div' ).last().remove();
+        $("#bahia").addClass("show");
+        },
+        function() {
+        $( "#bahia" ).removeClass("show");
+      }
+    );
+    
+    $("#ce").hover( 
+        function() {
+        console.log('text');
+        $("#ceara").addClass("show");
+        },
+        function() {
+        $( "#ceara" ).removeClass("show");
+      }
+    );
+
+    $("#pe").hover( 
+        function() {
+        console.log('text');
+        $("#pernambuco").addClass("show");
+        },
+        function() {
+        $( "#pernambuco" ).removeClass("show");
+      }
+    );
+    
+    $("#pi").hover( 
+        function() {
+        console.log('text');
+        $("#piaui").addClass("show");
+        },
+        function() {
+        $( "#piaui" ).removeClass("show");
+      }
+    );
+    
+    $("#to").hover( 
+        function() {
+        console.log('text');
+        $("#tocantins").addClass("show");
+        },
+        function() {
+        $( "#tocantins" ).removeClass("show");
+      }
+    );
+    
+    $("#df").hover( 
+        function() {
+        console.log('text');
+        $("#dfederal").addClass("show");
+        },
+        function() {
+        $( "#dfederal" ).removeClass("show");
+      }
+    );
+    
+    $("#mg").hover( 
+        function() {
+        console.log('text');
+        $("#minas").addClass("show");
+        },
+        function() {
+        $( "#minas" ).removeClass("show");
+      }
+    );
+    
+    $("#mt").hover( 
+        function() {
+        console.log('text');
+        $("#matogrosso").addClass("show");
+        },
+        function() {
+        $( "#matogrosso" ).removeClass("show");
+      }
+    );
+    
+    $("#ms").hover( 
+        function() {
+        console.log('text');
+        $("#matogrossodosul").addClass("show");
+        },
+        function() {
+        $( "#matogrossodosul" ).removeClass("show");
+      }
+    );
+    
+    $("#rj").hover( 
+        function() {
+        console.log('text');
+        $("#rio").addClass("show");
+        },
+        function() {
+        $( "#rio" ).removeClass("show");
+      }
+    );
+
+    $("#sp").hover( 
+        function() {
+        console.log('text');
+        $("#saopaulo").addClass("show");
+        },
+        function() {
+        $( "#saopaulo" ).removeClass("show");
+      }
+    );
+
+    $("#pr").hover( 
+        function() {
+        console.log('text');
+        $("#parana").addClass("show");
+        },
+        function() {
+        $( "#parana" ).removeClass("show");
+      }
+    );
+
+    $("#sc").hover( 
+        function() {
+        console.log('text');
+        $("#stacatarina").addClass("show");
+        },
+        function() {
+        $( "#stacatarina" ).removeClass("show");
+      }
+    );
+    $("#rs").hover( 
+        function() {
+        console.log('text');
+        $("#riogdosul").addClass("show");
+        },
+        function() {
+        $( "#riogdosul" ).removeClass("show");
       }
     );
     
